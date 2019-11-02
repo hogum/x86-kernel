09fe8b67972ab496c9999a4fbcf537354fe8a0f6
mugoh <mugoh.ks@gmail.com>
2019-11-02 21:58:45 +0300

    link kernel and bootloader

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,17 @@ use core::panic::PanicInfo;
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
-
+static VISUAL: &[u8] = b"Underppined legs";
 #[no_mangle]
 /// Linker entry point
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in VISUAL.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
