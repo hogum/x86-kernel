4a3f20efc797f3554d2c4a92eead277f260a2fe8
mugoh <mugoh.ks@gmail.com>
2019-11-09 19:17:04 +0300

    make volatile writes to buffer

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,4 +1,12 @@
 //! Handles printing to VGA
+use core::fmt::{self, Write};
+use volatile::Volatile;
+
+pub static Writer: ScreenWriter = ScreenWriter {
+    column_position: 0,
+    colour_code: ColourCode::new(Colour::Yellow, Colour::Black),
+    buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+};
 
 /// Colour bit variants
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
@@ -47,7 +55,7 @@ const BUFFER_WIDTH: usize = 25;
 const BUFFER_HEIGHT: usize = 80;
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
 /// Writes to Screen
@@ -68,12 +76,11 @@ impl ScreenWriter {
                 }
                 let row = BUFFER_HEIGHT - 1;
                 let col = self.column_position;
-                let colour_code = self.colour_code;
 
-                self.buffer.chars[row][col] = ScreenChar {
+                self.buffer.chars[row][col].write(ScreenChar {
                     ascii_char: byte,
-                    colour_code,
-                };
+                    colour_code: self.colour_code,
+                });
                 self.column_position += 1;
             }
         }
@@ -89,8 +96,35 @@ impl ScreenWriter {
         }
     }
     /// Shifts row lines up by wrapping the current line
+    /// Iterates over all characters, shifting them a row up
     fn new_line(&mut self) {
-        //
+        for row in 1..BUFFER_HEIGHT {
+            for col in 1..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+            self.clear_row(BUFFER_HEIGHT - 1);
+            self.column_position = 0;
+        }
+    }
+    fn clear_row(&mut self, row: usize) -> () {
+        let blank = ScreenChar {
+            ascii_char: b' ',
+            colour_code: self.colour_code,
+        };
+
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+}
+
+/// Implement formatting macros
+/// to allow printing of types
+impl fmt::Write for ScreenWriter {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_str(s);
+        Ok(())
     }
 }
 
@@ -105,4 +139,5 @@ pub fn chekout_print() -> () {
     writer.write_byte(b'I');
     writer.write_string("chigyIchigy");
     writer.write_string(" gū");
+    write!(writer, "The odds might be {} against {}", 39, 3 / 33).unwrap();
 }
