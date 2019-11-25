21f715033cf51181a7ce30e5b5bef07867268f4a
mugoh <mugoh.ks@gmail.com>
2019-11-25 21:17:26 +0300

    add breakpoint handler

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,7 +1,12 @@
 /// Interrupts
-use x86_64::structures::idt::InterruptDescriptorTable;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
 /// Creates the IDT
 pub fn init_idt() -> () {
     let mut idt = InterruptDescriptorTable::new();
+    idt.breakpoint.set_handler_fn(breakpoint_handler);
+}
+
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut InterruptStackFrame) {
+    println!("Oops! Exception:\n\t{:#?}", stack_frame);
 }
