2cfc119452937908437b8e46a0bedb74b74edbd7
mugoh <mugoh.ks@gmail.com>
2019-12-14 21:07:59 +0300

    lookup scancode set

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -84,7 +84,25 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: &mut Interrup
 
     // Read data from PS/2 controller: port number 0x60
     let mut port = Port::new(0x60);
-    let scancode: u8 = unsafe{ port.read(); }
+    let scancode: u8 = unsafe { port.read() };
+    let key = match scancode {
+        0x01 => Some('E'),
+        0x02 => Some('1'),
+        0x03 => Some('2'),
+        0x04 => Some('3'),
+        0x05 => Some('4'),
+        0x06 => Some('5'),
+        0x07 => Some('6'),
+        0x08 => Some('7'),
+        0x09 => Some('8'),
+        0x0a => Some('9'),
+        0x0b => Some('0'),
+        _ => None,
+    };
+
+    if let Some(key) = key {
+        println!("{}", key);
+    }
     println!("{}", scancode);
 
     unsafe {
