6ca1416bac5120fbe80fa74c4edbeafcd681b324
mugoh <mugoh.ks@gmail.com>
2020-01-08 17:48:38 +0300

    setup heap integration tests

---
+++ tests/allocation.rs
@@ -0,0 +1,38 @@
+//! Heap Allocation tests
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(x86_kernel::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+extern crate alloc;
+
+use bootloader::{entry_point, BootInfo};
+use core::{panic, PanicInfo};
+
+entry_point!(main);
+
+/// Heap test entry point
+fn main(_boot_info: &'static BootInfo) -> ! {
+    use x86_kernel::{
+        allocator,
+        memory::{self, BootInfoFrameAllocator},
+    };
+
+    use x86_64::VirtAddr;
+
+    x86_kernel::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+    allocator::map_heap(&mut mapper, &mut frame_allocator).expect("Failed to initialize heap");
+    test_main();
+    loop {}
+}
+
+#[panic_handler]
+/// Test panic handler
+fn panic(info: &PanicInfo) -> ! {
+    x86_kernel::test_panic_handler(info)
+}
